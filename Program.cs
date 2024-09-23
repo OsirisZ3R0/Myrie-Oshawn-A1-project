7e5774ceac0d5cfe537363910279d3253247b779
teesh <teesh@OSIRIS>
2024-09-23 13:35:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escape Doom's lair");
+Console.WriteLine("Iron Man:");
+
+string input = Console.ReadLine();
+
+Console.WriteLine($"Iron Man said {input}");
+
+
+
+
+
+
+
